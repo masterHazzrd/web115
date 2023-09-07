0e2099c9c0aac32e55204b2eb1fa3e60492542ff
ManWithNoName <118701860+masterHazzrd@users.noreply.github.com>
2023-09-07 18:37:20 +0000

    added js file jg

---
+++ script/changeimage.js
@@ -0,0 +1,17 @@
+// Get references to the image and container
+const image = document.getElementById("changeimage");
+const imageContainer = document.getElementsByClassName("zoom");
+
+// Define the new image source for when hovering
+const newImageSrc = "";
+
+// Add event listeners for mouse hover and mouse out
+imageContainer.addEventListener("mouseover", function () {
+    // Change the image source on hover
+    image.src = newImageSrc;
+});
+
+imageContainer.addEventListener("mouseout", function () {
+    // Restore the original image source on mouse out
+    image.src = "original-image.jpg";
+});
