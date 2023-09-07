5a799110ce6376da27de357d20985fe886a4e397
ManWithNoName <118701860+masterHazzrd@users.noreply.github.com>
2023-09-07 19:51:04 +0000

    jg

--- script/changeimage.js
+++ script/changeimage.js
@@ -1,17 +1,12 @@
-// Get references to the image and container
-const imageContainer = document.getElementsById("zoom");
-const image = document.getElementById("changeimage");
+const imgcontainer = document.getElementById("zoom");
+const img = document.getElementById("changeimg");
 
-// Define the new image source for when hovering
-const newImageSrc = "/web115/images/meinfrontofsign.jpg";
+const newImgSrc = "/web115/images/meinfrontofsign.jpg";
 
-// Add event listeners for mouse hover and mouse out
-imageContainer.addEventListener("mouseover", function () {
-    // Change the image source on hover
-    image.src = newImageSrc;
+imgcontainer.addEventListener("mouseover", function chngImg() {
+    img.src = newImgSrc;
 });
 
-imageContainer.addEventListener("mouseout", function () {
-    // Restore the original image source on mouse out
-    image.src = "/workspaces/web115/images/army_me.jpg";
+imgcontainer.addEventListener("mouseout", function chngImg() {
+    img.src = "/web115/images/army_me.jpg";
 });
