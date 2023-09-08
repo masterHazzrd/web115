7612ef1ab2ae1c784473ea8722af27f5d86c8851
ManWithNoName <118701860+masterHazzrd@users.noreply.github.com>
2023-09-08 21:45:37 +0000

    jg

--- script/changeimage.js
+++ script/changeimage.js
@@ -1,7 +1,7 @@
 const imgcontainer = document.getElementById("zoom");
 const img = document.getElementById("changeimg");
 
-const originalImg = img.scr;
+const originalImg = "../web115/images/army_me.jpg";
 
 const newImgSrc = "../web115/images/meinfrontofsign.jpg";
 
