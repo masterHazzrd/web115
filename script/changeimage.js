872cb02529c68e3a72bf45a3117aec21d913d1fc
ManWithNoName <118701860+masterHazzrd@users.noreply.github.com>
2023-09-08 21:44:47 +0000

    jg

--- script/changeimage.js
+++ script/changeimage.js
@@ -3,14 +3,14 @@ const img = document.getElementById("changeimg");
 
 const originalImg = img.scr;
 
-const newImgSrc = "/web115/images/meinfrontofsign.jpg";
+const newImgSrc = "../web115/images/meinfrontofsign.jpg";
 
 imgcontainer.addEventListener("mouseover", function chngImg() {
     img.src = newImgSrc;
 });
 
 imgcontainer.addEventListener("mouseout", function chngImg() {
-    img.src = "/web115/images/meandwifeyatconcert.jpg";
+    img.src = "../web115/images/meandwifeyatconcert.jpg";
 });
 
 imgcontainer.addEventListener("load", function chngImg() {
