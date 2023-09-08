abca7c9fd4874c27cbd824d0afab1dd93c8e4515
ManWithNoName <118701860+masterHazzrd@users.noreply.github.com>
2023-09-08 21:30:45 +0000

    change js to auto change jg

--- script/changeimage.js
+++ script/changeimage.js
@@ -1,20 +1,28 @@
-const imgcontainer = document.getElementById("zoom");
-const img = document.getElementById("changeimg");
+const images = [
+    "/web115/images/meinfrontofsign.jpg",
+    "/web115/images/biohard_skull_2.jpg",
+    "/web115/images/meandwifeyatconcert.jpg",
+    "/web115/images/biohazard_skul.jpg",
+    // Add more image URLs here
+];
 
-const originalImg = img.scr;
+const imgChngr = document.getElementById("changeimg");
+let currentIndex = 0;
 
-const newImgSrc = "/web115/images/meinfrontofsign.jpg";
+function changeImage() {
+    imgChngr.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+}
 
-imgcontainer.addEventListener("mouseover", function chngImg() {
-    img.src = newImgSrc;
-});
+// Change image every 3 seconds (adjust the time interval as needed)
+const interval = setInterval(changeImage, 3000);
 
-imgcontainer.addEventListener("mouseout", function chngImg() {
-    img.src = "/web115/images/meandwifeyatconcert.jpg";
-});
-
-imgcontainer.addEventListener("load", function chngImg() {
-    setTimeout(function chngImg() {
-        img.src = originalImg;
-    }, 3000);
+// Reset to the first image after going through all images
+imgChngr.addEventListener("load", function () {
+    clearInterval(interval);
+    setTimeout(function () {
+        currentIndex = 0;
+        changeImage();
+        interval = setInterval(changeImage, 3000);
+    }, 3000); // Delay before resetting (3 seconds in this example)
 });
