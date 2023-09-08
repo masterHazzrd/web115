07dd420b31efec4c92935a3f6074085844c5a306
ManWithNoName <118701860+masterHazzrd@users.noreply.github.com>
2023-09-08 22:29:01 +0000

    added new js script jg

---
+++ script/imgTimer.js
@@ -0,0 +1,10 @@
+var Imgs = ["../web115/images/army_me.jpg", "../web115/images/biohard_skull_2.jpg", "./web115/images/meinfrontofsign.jpg", "../web115/images/biohazard_skull.jpg", "../web115/images/meandwifeyatconcert.jpg"]
+
+var index = 0;
+setInterval (function(){
+  if (index == Imgs.length) {
+    index = 0;
+  }
+  document.getElementById("chgImg").src = Imgs[index];
+  index++;
+} , 4000);
