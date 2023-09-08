041721ef8fe89498f91e64d0f829479d351199ad
ManWithNoName <118701860+masterHazzrd@users.noreply.github.com>
2023-09-08 22:43:04 +0000

    jg

--- script/imgTimer.js
+++ script/imgTimer.js
@@ -1,4 +1,4 @@
-var Imgs = ["../web115/images/biohard_skull_2.jpg", "../web115/images/meinfrontofsign.jpg", "../web115/images/biohazard_skull.jpg", "../web115/images/meandwifeyatconcert.jpg"]
+var Imgs = ["../web115/images/army_me.jpg", "../web115/images/biohard_skull_2.jpg", "../web115/images/meinfrontofsign.jpg", "../web115/images/biohazard_skull.jpg", "../web115/images/meandwifeyatconcert.jpg"]
 
 var index = 0;
 setInterval (function(){
