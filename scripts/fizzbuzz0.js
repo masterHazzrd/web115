9b78d4888bb613789955125d4f7cf501400e0c0a
ManWithNoName <warpd.hazzrd@gmail.com>
2023-11-28 05:13:26 -0500

    jg

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,21 +1,25 @@
 function pageModification() {
 
-  const f_name = document.getElementById("f-name");
-  const m_init = document.getElementById("m-initial");
-  const l_name = document.getElementById("l-name");
+  const f_name = document.getElementById('f-name').value;
+  const m_init = document.getElementById('m-initial').value;
+  const l_name = document.getElementById('l-name').value;
 
   const new_header = `Welcome! fellow, <strong id="fb-greeting">CleanEnergySmithers</strong>, ${f_name} ${m_init ? ' ' + middleInitial + '.' : ''} ${l_name}!`;
 
-  document.getElementById("greeting").textContent = new_header;
+  document.getElementById('greeting').textContent = new_header;
 
-  const number = document.getElementById("number");
+  const scrnMessage = document.getElementById('scrnMessag');
+
+  scrnMessage.innerHTML = '';
+
+  const number = document.getElementById('number');
   const count = parseInt(number);
 
   for (let i = 1; i <= count; i++) {
     const reply = document.createElement('li');
 
-    reply.textContent = `${i} - Wattage number is ${i % 2 === 0 ? 'even' : 'odd'}`;
+    reply.textContent = `${i} - Wattage number ${i % 2 === 0 ? 'is even' : 'is odd'}`;
 
-    output.appendChild(reply);
+    scrnMessage.append(reply);
   }
 }
