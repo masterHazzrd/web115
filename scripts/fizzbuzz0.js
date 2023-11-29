16f9c984975947b238f7584c8a87994c98888e48
ManWithNoName <warpd.hazzrd@gmail.com>
2023-11-28 20:51:02 -0500

    JG

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,25 +1,5 @@
+document.addEventListener("click", )
+
 function pageModification() {
-
-  const f_name = document.getElementById('f-name').value;
-  const m_init = document.getElementById('m-initial').value;
-  const l_name = document.getElementById('l-name').value;
-
-  const new_header = `Welcome! fellow, <strong id="fb-greeting">CleanEnergySmithers</strong>, ${f_name} ${m_init ? ' ' + middleInitial + '.' : ''} ${l_name}!`;
-
-  document.getElementById('greeting').textContent = new_header;
-
-  const scrnMessage = document.getElementById('scrnMessag');
-
-  scrnMessage.innerHTML = '';
-
-  const number = document.getElementById('number');
-  const count = parseInt(number);
-
-  for (let i = 1; i <= count; i++) {
-    const reply = document.createElement('li');
-
-    reply.textContent = `${i} - Wattage number ${i % 2 === 0 ? 'is even' : 'is odd'}`;
-
-    scrnMessage.append(reply);
-  }
+  const  name = document.getElementById(f-name, m-initial, l-name).innerHTML = `Welcome fellow, <strong class="fb-greeting">CleanEnergySmithers</strong>! ${name}`
 }
